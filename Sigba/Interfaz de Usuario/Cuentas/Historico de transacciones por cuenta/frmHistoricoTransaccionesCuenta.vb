57640cda69b96e5c3224dfe7076afc95a2130ed3
Franco Correa <fcorrea@sophilabs.com>
2017-10-15 15:58:34 -0300

    Quitar metodo innecesario

--- Sigba/Interfaz de Usuario/Cuentas/Historico de transacciones por cuenta/frmHistoricoTransaccionesCuenta.vb
+++ Sigba/Interfaz de Usuario/Cuentas/Historico de transacciones por cuenta/frmHistoricoTransaccionesCuenta.vb
@@ -5,7 +5,4 @@
         txtNroCuenta.Text = nroCuenta
     End Sub
 
-    Private Sub frmHistoricoTransaccionesCuenta_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        
-    End Sub
 End Class
