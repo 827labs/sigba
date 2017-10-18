3fab60652df89d3be160a2e853077a04d68339d3
Franco Correa <fcorrea@sophilabs.com>
2017-10-18 12:30:03 -0300

    Traduccion ingreso cotizacion

--- Sigba/Interfaz de Usuario/Configuracion/frmSeteoCotizacionManual.Designer.vb
+++ Sigba/Interfaz de Usuario/Configuracion/frmSeteoCotizacionManual.Designer.vb
@@ -22,6 +22,7 @@ Partial Class frmSeteoCotizacionManual
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmSeteoCotizacionManual))
         Me.Label1 = New System.Windows.Forms.Label
         Me.Label2 = New System.Windows.Forms.Label
         Me.txtUSDCot = New System.Windows.Forms.TextBox
@@ -32,61 +33,64 @@ Partial Class frmSeteoCotizacionManual
         '
         'Label1
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(10, 11)
+        Me.Label1.AccessibleDescription = Nothing
+        Me.Label1.AccessibleName = Nothing
+        resources.ApplyResources(Me.Label1, "Label1")
+        Me.Label1.Font = Nothing
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(327, 26)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Necesitamos que ingrese la cotización del día de hoy a fin de poder" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "operar norma" & _
-            "lmente en todos los servicios del banco."
         '
         'Label2
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(13, 51)
+        Me.Label2.AccessibleDescription = Nothing
+        Me.Label2.AccessibleName = Nothing
+        resources.ApplyResources(Me.Label2, "Label2")
+        Me.Label2.Font = Nothing
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(101, 13)
-        Me.Label2.TabIndex = 1
-        Me.Label2.Text = "Dólares Americanos"
         '
         'txtUSDCot
         '
-        Me.txtUSDCot.Location = New System.Drawing.Point(16, 68)
+        Me.txtUSDCot.AccessibleDescription = Nothing
+        Me.txtUSDCot.AccessibleName = Nothing
+        resources.ApplyResources(Me.txtUSDCot, "txtUSDCot")
+        Me.txtUSDCot.BackgroundImage = Nothing
+        Me.txtUSDCot.Font = Nothing
         Me.txtUSDCot.Name = "txtUSDCot"
-        Me.txtUSDCot.Size = New System.Drawing.Size(154, 20)
-        Me.txtUSDCot.TabIndex = 2
         '
         'txtEURCot
         '
-        Me.txtEURCot.Location = New System.Drawing.Point(176, 68)
+        Me.txtEURCot.AccessibleDescription = Nothing
+        Me.txtEURCot.AccessibleName = Nothing
+        resources.ApplyResources(Me.txtEURCot, "txtEURCot")
+        Me.txtEURCot.BackgroundImage = Nothing
+        Me.txtEURCot.Font = Nothing
         Me.txtEURCot.Name = "txtEURCot"
-        Me.txtEURCot.Size = New System.Drawing.Size(161, 20)
-        Me.txtEURCot.TabIndex = 4
         '
         'Label3
         '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(173, 51)
+        Me.Label3.AccessibleDescription = Nothing
+        Me.Label3.AccessibleName = Nothing
+        resources.ApplyResources(Me.Label3, "Label3")
+        Me.Label3.Font = Nothing
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(34, 13)
-        Me.Label3.TabIndex = 3
-        Me.Label3.Text = "Euros"
         '
         'btnContinuar
         '
-        Me.btnContinuar.Location = New System.Drawing.Point(131, 103)
+        Me.btnContinuar.AccessibleDescription = Nothing
+        Me.btnContinuar.AccessibleName = Nothing
+        resources.ApplyResources(Me.btnContinuar, "btnContinuar")
+        Me.btnContinuar.BackgroundImage = Nothing
+        Me.btnContinuar.Font = Nothing
         Me.btnContinuar.Name = "btnContinuar"
-        Me.btnContinuar.Size = New System.Drawing.Size(95, 23)
-        Me.btnContinuar.TabIndex = 5
-        Me.btnContinuar.Text = "Continuar"
         Me.btnContinuar.UseVisualStyleBackColor = True
         '
         'frmSeteoCotizacionManual
         '
         Me.AcceptButton = Me.btnContinuar
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AccessibleDescription = Nothing
+        Me.AccessibleName = Nothing
+        resources.ApplyResources(Me, "$this")
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(352, 138)
+        Me.BackgroundImage = Nothing
         Me.ControlBox = False
         Me.Controls.Add(Me.btnContinuar)
         Me.Controls.Add(Me.txtEURCot)
@@ -94,12 +98,13 @@ Partial Class frmSeteoCotizacionManual
         Me.Controls.Add(Me.txtUSDCot)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
+        Me.Font = Nothing
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = Nothing
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "frmSeteoCotizacionManual"
         Me.ShowIcon = False
-        Me.Text = "Asignación de cotización manual"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
