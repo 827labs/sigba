993554aaf72ffc0541fbd92fafadccf3f02fc7ea
Franco Correa <fcorrea@sophilabs.com>
2017-10-18 12:56:03 -0300

    Traduccion transferencia

--- Sigba/Interfaz de Usuario/Cuentas/frmTransferenciaCuentas.Designer.vb
+++ Sigba/Interfaz de Usuario/Cuentas/frmTransferenciaCuentas.Designer.vb
@@ -49,204 +49,203 @@ Partial Class frmTransferenciaCuentas
         '
         'GroupBox1
         '
+        Me.GroupBox1.AccessibleDescription = Nothing
+        Me.GroupBox1.AccessibleName = Nothing
+        resources.ApplyResources(Me.GroupBox1, "GroupBox1")
+        Me.GroupBox1.BackgroundImage = Nothing
         Me.GroupBox1.Controls.Add(Me.lblSaldoCuentaOrigen)
         Me.GroupBox1.Controls.Add(Me.lblTitularCtaOrigen)
         Me.GroupBox1.Controls.Add(Me.Label2)
         Me.GroupBox1.Controls.Add(Me.Label1)
         Me.GroupBox1.Controls.Add(Me.btnBuscarCuentaOrigen)
         Me.GroupBox1.Controls.Add(Me.txtCuentaOrigen)
-        Me.GroupBox1.Location = New System.Drawing.Point(13, 13)
+        Me.GroupBox1.Font = Nothing
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(259, 89)
-        Me.GroupBox1.TabIndex = 0
         Me.GroupBox1.TabStop = False
-        Me.GroupBox1.Text = "Cuenta de origen"
         '
         'lblSaldoCuentaOrigen
         '
-        Me.lblSaldoCuentaOrigen.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblSaldoCuentaOrigen.Location = New System.Drawing.Point(96, 62)
+        Me.lblSaldoCuentaOrigen.AccessibleDescription = Nothing
+        Me.lblSaldoCuentaOrigen.AccessibleName = Nothing
+        resources.ApplyResources(Me.lblSaldoCuentaOrigen, "lblSaldoCuentaOrigen")
         Me.lblSaldoCuentaOrigen.Name = "lblSaldoCuentaOrigen"
-        Me.lblSaldoCuentaOrigen.Size = New System.Drawing.Size(157, 13)
-        Me.lblSaldoCuentaOrigen.TabIndex = 5
-        Me.lblSaldoCuentaOrigen.Text = "UYU 123,45"
-        Me.lblSaldoCuentaOrigen.TextAlign = System.Drawing.ContentAlignment.MiddleRight
         '
         'lblTitularCtaOrigen
         '
-        Me.lblTitularCtaOrigen.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblTitularCtaOrigen.Location = New System.Drawing.Point(48, 46)
+        Me.lblTitularCtaOrigen.AccessibleDescription = Nothing
+        Me.lblTitularCtaOrigen.AccessibleName = Nothing
+        resources.ApplyResources(Me.lblTitularCtaOrigen, "lblTitularCtaOrigen")
         Me.lblTitularCtaOrigen.Name = "lblTitularCtaOrigen"
-        Me.lblTitularCtaOrigen.Size = New System.Drawing.Size(205, 13)
-        Me.lblTitularCtaOrigen.TabIndex = 4
-        Me.lblTitularCtaOrigen.Text = "APELLIDO, NOMBRE"
-        Me.lblTitularCtaOrigen.TextAlign = System.Drawing.ContentAlignment.MiddleRight
         '
         'Label2
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(3, 62)
+        Me.Label2.AccessibleDescription = Nothing
+        Me.Label2.AccessibleName = Nothing
+        resources.ApplyResources(Me.Label2, "Label2")
+        Me.Label2.Font = Nothing
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(87, 13)
-        Me.Label2.TabIndex = 3
-        Me.Label2.Text = "Saldo disponible:"
         '
         'Label1
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(3, 46)
+        Me.Label1.AccessibleDescription = Nothing
+        Me.Label1.AccessibleName = Nothing
+        resources.ApplyResources(Me.Label1, "Label1")
+        Me.Label1.Font = Nothing
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(39, 13)
-        Me.Label1.TabIndex = 2
-        Me.Label1.Text = "Titular:"
         '
         'btnBuscarCuentaOrigen
         '
-        Me.btnBuscarCuentaOrigen.Location = New System.Drawing.Point(200, 18)
+        Me.btnBuscarCuentaOrigen.AccessibleDescription = Nothing
+        Me.btnBuscarCuentaOrigen.AccessibleName = Nothing
+        resources.ApplyResources(Me.btnBuscarCuentaOrigen, "btnBuscarCuentaOrigen")
+        Me.btnBuscarCuentaOrigen.BackgroundImage = Nothing
+        Me.btnBuscarCuentaOrigen.Font = Nothing
         Me.btnBuscarCuentaOrigen.Name = "btnBuscarCuentaOrigen"
-        Me.btnBuscarCuentaOrigen.Size = New System.Drawing.Size(53, 23)
-        Me.btnBuscarCuentaOrigen.TabIndex = 1
-        Me.btnBuscarCuentaOrigen.Text = "Buscar"
         Me.btnBuscarCuentaOrigen.UseVisualStyleBackColor = True
         '
         'txtCuentaOrigen
         '
-        Me.txtCuentaOrigen.Location = New System.Drawing.Point(6, 19)
+        Me.txtCuentaOrigen.AccessibleDescription = Nothing
+        Me.txtCuentaOrigen.AccessibleName = Nothing
+        resources.ApplyResources(Me.txtCuentaOrigen, "txtCuentaOrigen")
+        Me.txtCuentaOrigen.BackgroundImage = Nothing
+        Me.txtCuentaOrigen.Font = Nothing
         Me.txtCuentaOrigen.Name = "txtCuentaOrigen"
-        Me.txtCuentaOrigen.Size = New System.Drawing.Size(189, 20)
-        Me.txtCuentaOrigen.TabIndex = 0
         '
         'GroupBox2
         '
+        Me.GroupBox2.AccessibleDescription = Nothing
+        Me.GroupBox2.AccessibleName = Nothing
+        resources.ApplyResources(Me.GroupBox2, "GroupBox2")
+        Me.GroupBox2.BackgroundImage = Nothing
         Me.GroupBox2.Controls.Add(Me.lblTitularCtaDestino)
         Me.GroupBox2.Controls.Add(Me.Label8)
         Me.GroupBox2.Controls.Add(Me.btnBuscarCuentaDestino)
         Me.GroupBox2.Controls.Add(Me.txtCuentaDestino)
-        Me.GroupBox2.Location = New System.Drawing.Point(13, 108)
+        Me.GroupBox2.Font = Nothing
         Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(259, 68)
-        Me.GroupBox2.TabIndex = 6
         Me.GroupBox2.TabStop = False
-        Me.GroupBox2.Text = "Cuenta de destino"
         '
         'lblTitularCtaDestino
         '
-        Me.lblTitularCtaDestino.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblTitularCtaDestino.Location = New System.Drawing.Point(48, 46)
+        Me.lblTitularCtaDestino.AccessibleDescription = Nothing
+        Me.lblTitularCtaDestino.AccessibleName = Nothing
+        resources.ApplyResources(Me.lblTitularCtaDestino, "lblTitularCtaDestino")
         Me.lblTitularCtaDestino.Name = "lblTitularCtaDestino"
-        Me.lblTitularCtaDestino.Size = New System.Drawing.Size(205, 13)
-        Me.lblTitularCtaDestino.TabIndex = 4
-        Me.lblTitularCtaDestino.Text = "APELLIDO, NOMBRE"
-        Me.lblTitularCtaDestino.TextAlign = System.Drawing.ContentAlignment.MiddleRight
         '
         'Label8
         '
-        Me.Label8.AutoSize = True
-        Me.Label8.Location = New System.Drawing.Point(3, 46)
+        Me.Label8.AccessibleDescription = Nothing
+        Me.Label8.AccessibleName = Nothing
+        resources.ApplyResources(Me.Label8, "Label8")
+        Me.Label8.Font = Nothing
         Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(39, 13)
-        Me.Label8.TabIndex = 2
-        Me.Label8.Text = "Titular:"
         '
         'btnBuscarCuentaDestino
         '
-        Me.btnBuscarCuentaDestino.Location = New System.Drawing.Point(200, 18)
+        Me.btnBuscarCuentaDestino.AccessibleDescription = Nothing
+        Me.btnBuscarCuentaDestino.AccessibleName = Nothing
+        resources.ApplyResources(Me.btnBuscarCuentaDestino, "btnBuscarCuentaDestino")
+        Me.btnBuscarCuentaDestino.BackgroundImage = Nothing
+        Me.btnBuscarCuentaDestino.Font = Nothing
         Me.btnBuscarCuentaDestino.Name = "btnBuscarCuentaDestino"
-        Me.btnBuscarCuentaDestino.Size = New System.Drawing.Size(53, 23)
-        Me.btnBuscarCuentaDestino.TabIndex = 3
-        Me.btnBuscarCuentaDestino.Text = "Buscar"
         Me.btnBuscarCuentaDestino.UseVisualStyleBackColor = True
         '
         'txtCuentaDestino
         '
-        Me.txtCuentaDestino.Location = New System.Drawing.Point(6, 19)
+        Me.txtCuentaDestino.AccessibleDescription = Nothing
+        Me.txtCuentaDestino.AccessibleName = Nothing
+        resources.ApplyResources(Me.txtCuentaDestino, "txtCuentaDestino")
+        Me.txtCuentaDestino.BackgroundImage = Nothing
+        Me.txtCuentaDestino.Font = Nothing
         Me.txtCuentaDestino.Name = "txtCuentaDestino"
-        Me.txtCuentaDestino.Size = New System.Drawing.Size(189, 20)
-        Me.txtCuentaDestino.TabIndex = 2
         '
         'GroupBox3
         '
+        Me.GroupBox3.AccessibleDescription = Nothing
+        Me.GroupBox3.AccessibleName = Nothing
+        resources.ApplyResources(Me.GroupBox3, "GroupBox3")
+        Me.GroupBox3.BackgroundImage = Nothing
         Me.GroupBox3.Controls.Add(Me.txtMontoCantidad)
         Me.GroupBox3.Controls.Add(Me.cboMontoMoneda)
         Me.GroupBox3.Controls.Add(Me.Label3)
-        Me.GroupBox3.Location = New System.Drawing.Point(13, 183)
+        Me.GroupBox3.Font = Nothing
         Me.GroupBox3.Name = "GroupBox3"
-        Me.GroupBox3.Size = New System.Drawing.Size(259, 65)
-        Me.GroupBox3.TabIndex = 7
         Me.GroupBox3.TabStop = False
-        Me.GroupBox3.Text = "Detalles de la transferencia"
         '
         'txtMontoCantidad
         '
-        Me.txtMontoCantidad.Location = New System.Drawing.Point(73, 36)
+        Me.txtMontoCantidad.AccessibleDescription = Nothing
+        Me.txtMontoCantidad.AccessibleName = Nothing
+        resources.ApplyResources(Me.txtMontoCantidad, "txtMontoCantidad")
+        Me.txtMontoCantidad.BackgroundImage = Nothing
+        Me.txtMontoCantidad.Font = Nothing
         Me.txtMontoCantidad.Name = "txtMontoCantidad"
-        Me.txtMontoCantidad.Size = New System.Drawing.Size(180, 20)
-        Me.txtMontoCantidad.TabIndex = 5
         '
         'cboMontoMoneda
         '
+        Me.cboMontoMoneda.AccessibleDescription = Nothing
+        Me.cboMontoMoneda.AccessibleName = Nothing
+        resources.ApplyResources(Me.cboMontoMoneda, "cboMontoMoneda")
+        Me.cboMontoMoneda.BackgroundImage = Nothing
         Me.cboMontoMoneda.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cboMontoMoneda.Font = Nothing
         Me.cboMontoMoneda.FormattingEnabled = True
-        Me.cboMontoMoneda.Items.AddRange(New Object() {"UYU", "USD"})
-        Me.cboMontoMoneda.Location = New System.Drawing.Point(9, 36)
+        Me.cboMontoMoneda.Items.AddRange(New Object() {resources.GetString("cboMontoMoneda.Items"), resources.GetString("cboMontoMoneda.Items1")})
         Me.cboMontoMoneda.Name = "cboMontoMoneda"
-        Me.cboMontoMoneda.Size = New System.Drawing.Size(58, 21)
-        Me.cboMontoMoneda.TabIndex = 4
         '
         'Label3
         '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(6, 19)
+        Me.Label3.AccessibleDescription = Nothing
+        Me.Label3.AccessibleName = Nothing
+        resources.ApplyResources(Me.Label3, "Label3")
+        Me.Label3.Font = Nothing
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(37, 13)
-        Me.Label3.TabIndex = 0
-        Me.Label3.Text = "Monto"
         '
         'Label4
         '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(13, 255)
+        Me.Label4.AccessibleDescription = Nothing
+        Me.Label4.AccessibleName = Nothing
+        resources.ApplyResources(Me.Label4, "Label4")
+        Me.Label4.Font = Nothing
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(270, 39)
-        Me.Label4.TabIndex = 8
-        Me.Label4.Text = "En caso de transferir en una moneda distinta a una de" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "las cuentas, se realizará " & _
-            "la conversión en base a la tasa" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "aplicable al día de la fecha." & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
         '
         'btnTransferir
         '
-        Me.btnTransferir.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnTransferir.Location = New System.Drawing.Point(179, 314)
+        Me.btnTransferir.AccessibleDescription = Nothing
+        Me.btnTransferir.AccessibleName = Nothing
+        resources.ApplyResources(Me.btnTransferir, "btnTransferir")
+        Me.btnTransferir.BackgroundImage = Nothing
         Me.btnTransferir.Name = "btnTransferir"
-        Me.btnTransferir.Size = New System.Drawing.Size(96, 23)
-        Me.btnTransferir.TabIndex = 6
-        Me.btnTransferir.Text = "TRANSFERIR"
         Me.btnTransferir.UseVisualStyleBackColor = True
         '
         'btnCancelar
         '
-        Me.btnCancelar.Location = New System.Drawing.Point(98, 314)
+        Me.btnCancelar.AccessibleDescription = Nothing
+        Me.btnCancelar.AccessibleName = Nothing
+        resources.ApplyResources(Me.btnCancelar, "btnCancelar")
+        Me.btnCancelar.BackgroundImage = Nothing
+        Me.btnCancelar.Font = Nothing
         Me.btnCancelar.Name = "btnCancelar"
-        Me.btnCancelar.Size = New System.Drawing.Size(75, 23)
-        Me.btnCancelar.TabIndex = 7
-        Me.btnCancelar.Text = "Cancelar"
         Me.btnCancelar.UseVisualStyleBackColor = True
         '
         'frmTransferenciaCuentas
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AccessibleDescription = Nothing
+        Me.AccessibleName = Nothing
+        resources.ApplyResources(Me, "$this")
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 349)
+        Me.BackgroundImage = Nothing
         Me.Controls.Add(Me.btnCancelar)
         Me.Controls.Add(Me.btnTransferir)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.GroupBox3)
         Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.GroupBox1)
+        Me.Font = Nothing
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
-        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.MaximizeBox = False
         Me.Name = "frmTransferenciaCuentas"
-        Me.Text = "Transferencia entre cuentas"
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox1.PerformLayout()
         Me.GroupBox2.ResumeLayout(False)
